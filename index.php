961a056ace78d9579f4bc5f0848daf5391a00142
Méline <meline.darcy@laplateforme.io>
2022-01-08 13:56:08 +0100

    Create index.php
    Page index et Accueil (OK)

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <title>Accueil</title>
+</head>
+  <body>
+  <?php include("include/header.php") ?>
+  <main>
+    <h2>Bienvenue</h2>
+    <p>Sur ce site vous pouvez mettre un avis sur le jeux Cyberpunk. <br>
+  N'oubliez pas de vous connecter ou de vous inscrire si ce n'est pas deja fait.</p>
+</main>
+  <footer>
+  <?php include("include/footer.php") ?>
+</footer>
+   </body>
+</html>
