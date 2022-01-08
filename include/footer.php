bb0f2029ed5fdcb1df94a5aab77627bf78d981d0
Méline <meline.darcy@laplateforme.io>
2022-01-08 14:09:29 +0100

    Header et Footer (ok)

---
+++ include/footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+
+<body>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Site</h3>
+                        <ul>
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="#">Livre d'or</a></li>
+                            
+                        </ul>
+                    </div>
+
+                    <div class="col-md-6 item text">
+                        <h3>Méline Darcy</h3>
+                        <p>Sur ce site tout les avis du jeu Cyberpunk sont noter.</p>
+                                        </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-github"></i></a></div>
+                </div>
+                <p class="copyright">MD © 2022</p>
+            </div>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
